e97bb1e92e50468d9ad04cfbb245dca5ce80a3ea
Dave Whitmer <dave@provsystems.com>
2018-08-23 16:52:45 -0400

    Implement PR

--- lib/activerecord-multi-tenant/query_rewriter.rb
+++ lib/activerecord-multi-tenant/query_rewriter.rb
@@ -10,7 +10,7 @@ module MultiTenant
 
     def eql?(rhs)
       self.class == rhs.class &&
-        equality_fields.eql?(rhs.equality_fields)
+          equality_fields.eql?(rhs.equality_fields)
     end
 
     def hash
@@ -54,6 +54,15 @@ module MultiTenant
     end
   end
 
+  class MultiTenantSubquery < Arel::Table
+    attr_accessor :data_source
+
+    def initialize(data_source, as:, type_caster: nil)
+      super(as, type_caster: type_caster)
+      @data_source = data_source
+    end
+  end
+
   class ArelTenantVisitor < Arel::Visitors::DepthFirst
     def initialize(arel)
       super(Proc.new {})
@@ -75,7 +84,7 @@ module MultiTenant
       if o.left.is_a?(Arel::Attributes::Attribute)
         table_name = o.left.relation.table_name
         model = MultiTenant.multi_tenant_model_for_table(table_name)
-        @current_context.visited_handled_relation(o.left.relation) if model.present? && o.left.name.to_s == model.partition_key.to_s
+        @current_context.visited_handled_relation(o.left.relation) if model.present? && o.left.name == model.partition_key
       end
       super(o, *args)
     end
@@ -113,6 +122,7 @@ module MultiTenant
         end
       end
     end
+
     alias :visit_Arel_Nodes_FullOuterJoin :visit_Arel_Nodes_OuterJoin
     alias :visit_Arel_Nodes_RightOuterJoin :visit_Arel_Nodes_OuterJoin
 
@@ -146,16 +156,17 @@ module MultiTenant
   end
 
   class TenantEnforcementClause < Arel::Nodes::Node
-    attr_reader :tenant_attribute
-    def initialize(tenant_attribute)
+    attr_reader :tenant_attribute, :source_attribute
+    def initialize(tenant_attribute, source_attribute = nil)
       @tenant_attribute = tenant_attribute
+      @source_attribute = source_attribute
     end
 
     def to_s; to_sql; end
     def to_str; to_sql; end
 
     def to_sql(*)
-      if MultiTenant.current_tenant_id
+      if source_attribute || MultiTenant.current_tenant_id
         tenant_arel.to_sql
       else
         '1=1'
@@ -165,10 +176,11 @@ module MultiTenant
     private
 
     def tenant_arel
-      if defined?(Arel::Nodes::Quoted)
-        @tenant_attribute.eq(Arel::Nodes::Quoted.new(MultiTenant.current_tenant_id))
+      src = source_attribute || MultiTenant.current_tenant_id
+      if defined?(Arel::Nodes::Quoted) && src.is_a?(String)
+        @tenant_attribute.eq(Arel::Nodes::Quoted.new(src))
       else
-        @tenant_attribute.eq(MultiTenant.current_tenant_id)
+        @tenant_attribute.eq(src)
       end
     end
   end
@@ -185,69 +197,112 @@ module MultiTenant
     end
   end
 
-  module DatabaseStatements
-    def join_to_update(update, *args)
-      update = super(update, *args)
-      model = MultiTenant.multi_tenant_model_for_table(update.ast.relation.table_name)
-      if model.present? && !MultiTenant.with_write_only_mode_enabled?
-        update.where(MultiTenant::TenantEnforcementClause.new(model.arel_table[model.partition_key]))
+  module MultiTenantSubqueryVisitor
+    if ActiveRecord::VERSION::MAJOR > 4 || (ActiveRecord::VERSION::MAJOR == 4 && ActiveRecord::VERSION::MINOR >= 2)
+      def visit_MultiTenant_MultiTenantSubquery(o, collector)
+        collector << '('
+        collector << case o.data_source
+                     when String
+                       o.data_source
+                     when Arel::SelectManager
+                       o.data_source.to_sql
+                     else
+                       o.data_source.to_s
+                     end
+        collector << ") #{quote_table_name o.name}"
       end
-      update
+    else
+      def visit_MultiTenant_MultiTenantSubquerye(o, a = nil)
+        query = case o.data_source
+                when String
+                  o.data_source
+                when Arel::SelectManager
+                  o.data_source.to_sql
+                else
+                  o.data_source.to_s
+                end
+        "(#{query}) #{quote_table_name o.name}"
+      end
     end
+  end
 
-    def join_to_delete(delete, *args)
-      delete = super(delete, *args)
-      model = MultiTenant.multi_tenant_model_for_table(delete.ast.left.table_name)
-      if model.present? && !MultiTenant.with_write_only_mode_enabled?
-        delete.where(MultiTenant::TenantEnforcementClause.new(model.arel_table[model.partition_key]))
-      end
-      delete
-    end
-
-    if ActiveRecord::VERSION::MAJOR >= 5 && ActiveRecord::VERSION::MINOR >= 2
-      def update(arel, name = nil, binds = [])
-        model = MultiTenant.multi_tenant_model_for_table(arel.ast.relation.table_name)
-        if model.present? && !MultiTenant.with_write_only_mode_enabled?
-          arel.where(MultiTenant::TenantEnforcementClause.new(model.arel_table[model.partition_key]))
+  module SqlWorkarounds
+    # Converts SELECT DISTINCT foo, bar to GROUP BY foo, bar
+    # to support cross partition queries with eager loaded associations
+    def self.convert_distinct_to_group_by(arel)
+      ctx = arel.ast.cores.last
+      projections = ctx.projections.first
+      if ctx.set_quantifier.is_a?(Arel::Nodes::Distinct)
+        ctx.set_quantifier = nil
+        if projections.is_a?(String)
+          return arel.group(ctx.projections.first.gsub(/ AS.*/, ''))
         end
-        super(arel, name, binds)
       end
 
-      def delete(arel, name = nil, binds = [])
-        model = MultiTenant.multi_tenant_model_for_table(arel.ast.left.table_name)
-        if model.present? && !MultiTenant.with_write_only_mode_enabled?
-          arel.where(MultiTenant::TenantEnforcementClause.new(model.arel_table[model.partition_key]))
+      # Citus does not support cross-partition COUNT(DISTINCT) except
+      # For approximations using the HLL extension
+      # However, if the query can be converted into a distinct via GROUP BY
+      # We can convert it into SELECT COUNT(*) FROM (... GROUP BY ...)
+      if projections.is_a?(Arel::Nodes::Count) && projections.distinct
+        return arel if MultiTenant.use_hll_counts?
+        ctx.projections = projections.expressions
+        partition_keys = projections.expressions.map do |e|
+          model = MultiTenant.multi_tenant_model_for_table(e.try(:relation).try(:name))
+          model.arel_table[model.partition_key] if model
+        end.compact
+        if partition_keys
+          return MultiTenant::MultiTenantSubquery.new(
+              arel.group(projections.expressions + partition_keys),
+              as: 'countme'
+          ).project(Arel.star.count)
         end
-        super(arel, name, binds)
       end
+      arel
     end
   end
 end
 
-require 'active_record/connection_adapters/abstract_adapter'
-ActiveRecord::ConnectionAdapters::AbstractAdapter.prepend(MultiTenant::DatabaseStatements)
-
 Arel::Visitors::ToSql.include(MultiTenant::TenantValueVisitor)
+Arel::Visitors::ToSql.include(MultiTenant::MultiTenantSubqueryVisitor)
 
 require 'active_record/relation'
 module ActiveRecord
   module QueryMethods
     alias :build_arel_orig :build_arel
-    def build_arel(*args)
-      arel = build_arel_orig(*args)
+    def build_arel
+      arel = build_arel_orig
 
-      if MultiTenant.current_tenant_id && !MultiTenant.with_write_only_mode_enabled?
+      unless MultiTenant.with_write_only_mode_enabled?
+        # Get select source to be used in enforcement clause if it is a multi-tenant table
+        source_table = arel.source.left
+        source_model = MultiTenant.multi_tenant_model_for_table(source_table.table_name)
+        source_partition_key = source_model.try(:partition_key)
+
         visitor = MultiTenant::ArelTenantVisitor.new(arel)
         visitor.contexts.each do |context|
           node = context.arel_node
-          context.unhandled_relations.each do |relation|
+          next unless MultiTenant.current_tenant || node.is_a?(Arel::Nodes::Join)
+          relations = if MultiTenant.current_tenant
+                        context.unhandled_relations.uniq
+                      else
+                        context.known_relations.uniq
+                      end
+          relations.each do |relation|
             model = MultiTenant.multi_tenant_model_for_table(relation.arel_table.table_name)
-            enforcement_clause = MultiTenant::TenantEnforcementClause.new(relation.arel_table[model.partition_key])
-
             case node
             when Arel::Nodes::Join #Arel::Nodes::OuterJoin, Arel::Nodes::RightOuterJoin, Arel::Nodes::FullOuterJoin
+              next if relation.arel_table.name == source_table.name
+              enforcement_clause = if model.partition_key == source_partition_key
+                                     MultiTenant::TenantEnforcementClause.new(
+                                         relation.arel_table[model.partition_key],
+                                         source_table[source_partition_key]
+                                     )
+                                   else
+                                     MultiTenant::TenantEnforcementClause.new(relation.arel_table[model.partition_key])
+                                   end
               node.right.expr = node.right.expr.and(enforcement_clause)
             when Arel::Nodes::SelectCore
+              enforcement_clause = MultiTenant::TenantEnforcementClause.new(relation.arel_table[model.partition_key])
               if node.wheres.empty?
                 node.wheres = [enforcement_clause]
               else
@@ -259,8 +314,8 @@ module ActiveRecord
           end
         end
       end
-
-      arel
+      return arel if MultiTenant.current_tenant
+      MultiTenant::SqlWorkarounds.convert_distinct_to_group_by(arel)
     end
   end
 end
